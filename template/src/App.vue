f844728557df07e335150ffe5a876b83da2abfb1
chenyong <1015561170@qq.com>
2019-07-15 10:57:30 +0800

    feat: 项目目录结构模板建立

---
+++ template/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h1>hello world!</h1>
+		<h1>欢迎使用example-cli!</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'App'
+}
+</script>
+
+<style>
+h1 {
+	text-align: center;
+}
+</style>
